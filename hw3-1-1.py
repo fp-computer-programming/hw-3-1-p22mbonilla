c51d8c17851b9d81d6b0ac07095f52adf2f1fa65
p22mbonilla <p22mbonilla@fairfieldprep.org>
2021-09-28 10:21:26 -0400

    complete hw-1-1

---
+++ hw3-1-1.py
@@ -0,0 +1,12 @@
+# Author MB 09/28/2021
+
+num = int(input("enter a number"))
+
+if num % 2 == 0:
+    print("it is divisble by 2")
+
+if num % 3 == 0:
+    print("it is divisble by 3")
+
+if num % 5 == 0:
+    print("it is divisble by 5")
---
+++ hw3-1-3.py
@@ -0,0 +1,18 @@
+# Author MB 09/28/2021
+
+num = int(input("enter a number"))
+
+if num % 2 == 0:
+    print("it is divisble by 2")
+else:
+    print("it is not divisble by 2")
+
+if num % 3 == 0:
+    print("it is divisble by 3")
+else:
+    print("it is not divisble by 3")
+
+if num % 5 == 0:
+    print("it is divisble by 5")
+else:
+    print("it is not divisble by 5")
